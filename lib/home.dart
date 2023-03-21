3f6951ae7a40b67882e02403bc5674010dbbdd18
Codebox124 <rajcodes733@gmail.com>
2023-03-21 14:11:07 -0700

    added input and loting button

--- lib/home.dart
+++ lib/home.dart
@@ -14,7 +14,33 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text("Random Generetor"),
       ),
-      
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                label: Text("Minimum"),
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                label: Text("Minimum"),
+              ),
+            ),
+            FloatingActionButton(
+              onPressed: () {},
+              child: Icon(Icons.home),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
